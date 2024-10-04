1183999ffc04ce8ddec56572c102e0bbd28f85fd
DaraghFiorilla <daraghfiorillafpl@gmail.com>
2024-10-04 16:06:20 +0100

    Update Commit 2

---
+++ games-engines-2024/TPCamera.gd
@@ -0,0 +1,15 @@
+extends Node
+
+var target
+var player
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	#global_position = lerp(global_position, target.global_position, 5.0)
+	#look_at(player.global_position)
+	pass
