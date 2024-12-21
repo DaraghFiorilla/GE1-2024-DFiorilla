2b045616b7e0e0b40c2a6fa9417303af649b8344
DaraghFiorilla <daraghfiorillafpl@gmail.com>
2024-12-21 21:06:57 +0000

    XR scene setup
    Set up an XR scene which will be used by main scene to simulate XR. For some reason Godot refuses to render the camera, despite having worldenvironment and lighting, which is incredibly frustrating

--- games-engines-2024/scripts/XR_Main.gd
+++ games-engines-2024/scripts/XR_Main.gd
@@ -1,11 +1,11 @@
 extends Node3D
 
+var xr_interface: XRInterface
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	xr_interface = XRServer.find_interface("OpenXR")
+	
+	if xr_interface and xr_interface.is.initialized():
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
+		
+		get_viewport().use_xr = true
